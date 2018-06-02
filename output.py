f5c88829b62c5d15808a7db5de2b3bb4e18e9ee4
Shikhar Sharma <s.shikharcse@gmail.com>
2018-06-02 12:44:28 +0530

    Update output.py

--- output.py
+++ output.py
@@ -27,12 +27,12 @@ datestring = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
 file = open('dataOn_'+datestring+'.txt', "w")
 
 if model_1_score > model_2_score:
-   file.write("model_1 is better ")
-   file.write("model_1 score is")
+   file.write("model_1 is better \n")
+   file.write("model_1 score is \n")
    #file.write(model_1_score)
 elif model_1_score < model_2_score:
-    file.write("model_2 is better ")
-    file.write("model_2 score is")
+    file.write("model_2 is better \n")
+    file.write("model_2 score is \n")
     #file.write(model_2_score)
 else:
     file.write("Both model have equal accuracy")
